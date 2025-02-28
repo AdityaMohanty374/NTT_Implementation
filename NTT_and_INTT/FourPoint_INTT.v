5b05fcdbb8c4de07dd92e03f22242e4fd72efa74
Aditya Mohanty <109948376+AdityaMohanty374@users.noreply.github.com>
2025-02-28 21:46:53 +0530

    Updated FourPoint_INTT.v

--- NTT_and_INTT/FourPoint_INTT.v
+++ NTT_and_INTT/FourPoint_INTT.v
@@ -1,99 +1,73 @@
-module FourPoint_INTT(
-    input clk,
-    input reset,
-    input [16:0] in0,  //Individual input points, each 8 bits wide
-    input [16:0] in1,
-    input [16:0] in2,
-    input [16:0] in3,
-    output reg [16:0] out0,  //Individual output points, each 8 bits wide
-    output reg [16:0] out1,
-    output reg [16:0] out2,
-    output reg [16:0] out3
+//modified for ordered inputs ordered outputs
+
+module intt4point(
+    input clk, rst,
+    input [15:0] in0, in1, in2, in3,
+    output reg [15:0] out0, out1, out2, out3
 );
+    wire [15:0] q_val = 16'd7681;
+    wire [15:0] phim1 = 16'd1213;
+    wire [15:0] phim2 = 16'd4298;
+    wire [15:0] phim3 = 16'd5756;
+    wire [15:0] temp0, temp1, temp2, temp3;
+    wire [15:0] y0, y1, y2, y3;
 
-    // Modulus and primitive root
-    parameter q = 7681;
-    parameter phi_m = 1213;
-    parameter phi_m2 = 4298;
-    parameter phi_m3 = 5756; 
-    parameter n_inv = 0.25; //n = 4
+    //first stage
+    gsbf u0(.in_up(in0), .in_down(in2), .twf(phim1), .q(q_val), .out_up(temp0), .out_down(temp1));//bit reversed
+    gsbf u1(.in_up(in1), .in_down(in3), .twf(phim3), .q(q_val), .out_up(temp2), .out_down(temp3));//order output
 
-    // Internal registers
-    reg [16:0] x0, x1, x2, x3;
-    reg [16:0] y0, y1, y2, y3;
-    reg [16:0] w1, w2, w3;
-    reg [16:0] temp0, temp1, temp2, temp3;
+    //second stage
+    gsbf u2(.in_up(temp0), .in_down(temp2), .twf(phim2), .q(q_val), .out_up(y0), .out_down(y2));
+    gsbf u3(.in_up(temp1), .in_down(temp3), .twf(phim2), .q(q_val), .out_up(y1), .out_down(y3));
 
-    // Function for modular addition
-    function [16:0] mod_add;
-        input [16:0] a, b;
-        begin
-            mod_add = (a + b) % q;
-        end
-    endfunction
-
-    // Function for modular subtraction
-    function [16:0] mod_sub;
-        input [16:0] a, b;
-        begin
-            mod_sub = (a >= b) ? (a - b) : (q + a - b);
-        end
-    endfunction
-
-    // Function for modular multiplication
-    function [16:0] mod_mul;
-        input [16:0] a, b;
-        begin
-            mod_mul = (a * b) % q;
-        end
-    endfunction
-
-    // Butterfly operation
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            x0 <= 0;
-            x1 <= 0;
-            x2 <= 0;
-            x3 <= 0;
-            y0 <= 0;
-            y1 <= 0;
-            y2 <= 0;
-            y3 <= 0;
-            out0 <= 0;
-            out1 <= 0;
-            out2 <= 0;
-            out3 <= 0;
-        end else begin
-            // Load input values
-            x0 <= in0;
-            x1 <= in1;
-            x2 <= in2;
-            x3 <= in3;
-            
-            // Precompute powers of omega
-            w1 <= phi_m;
-            w2 <= phi_m2;
-            w3 <= phi_m3;
-            
-            // First stage of butterfly
-            temp0 <= mod_add(x0, x1)%q;
-            temp1 <= mod_mul(mod_sub(x0, x1), phi_m)%q;
-            temp2 <= mod_add(x2, x3)%q;
-            temp3 <= mod_mul(mod_sub(x2, x3), phi_m3)%q;
-            
-            
-            
-            // Second stage of butterfly
-            y0 <= mod_add(temp0, temp2);
-            y1 <= mod_add(temp1, temp3);
-            y2 <= mod_mul(mod_sub(temp0, temp2), phi_m2);
-            y3 <= mod_mul(mod_sub(temp1, temp3), phi_m2);
-            
-            // Store output values
-            out0 <= y0*n_inv;
-            out1 <= y1*n_inv;
-            out2 <= y2*n_inv;
-            out3 <= y3*n_inv;
-        end
+    always @(posedge clk) begin
+      if (rst) begin
+        out0 <= 0;
+        out1 <= 0;
+        out2 <= 0;
+        out3 <= 0;
+      end else begin
+        out0 <= y0 >> 2; //n_inv = 0.25
+        out1 <= y1 >> 2; //equivalent to div by 4
+        out2 <= y2 >> 2; //normal bit order output
+        out3 <= y3 >> 2; 
+      end
     end
+endmodule
+
+module gsbf(
+  input [15:0] in_up, in_down, twf, q,
+  output [15:0] out_up, out_down
+);
+  wire [15:0] temp;
+  modadd ma(.a(in_up), .b(in_down), .q(q), .y(out_up));
+  modsub ms(.a(in_up), .b(in_down), .q(q), .y(temp));
+  modmul mm(.a(temp), .b(twf), .q(q), .y(out_down));
+endmodule
+
+module modadd(
+    input [15:0] a, b, q,
+    output [15:0] y
+);
+    wire [16:0] sum;
+    assign sum = a + b;
+    assign y = (sum >= q) ? sum - q : sum;
+endmodule
+
+module modsub(
+    input [15:0] a, b, q,
+    output [15:0] y
+);
+    wire [16:0] qplusa;
+    assign qplusa = q + a;
+    assign y = (a >= b) ? (a - b) : (qplusa - b);
+endmodule
+
+module modmul(
+    input [15:0] a, b, q,
+    output [15:0] y
+);
+    wire [31:0] result;
+    assign result = a * b;
+    assign y = result % q;
 endmodule
