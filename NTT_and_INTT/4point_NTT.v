ecc50a652378fb43a181b5a6c1ee7a351f31ba75
Aditya Mohanty <109948376+AdityaMohanty374@users.noreply.github.com>
2025-02-28 21:46:18 +0530

    Updated 4point_NTT.v

--- NTT_and_INTT/4point_NTT.v
+++ NTT_and_INTT/4point_NTT.v
@@ -1,98 +1,71 @@
-module FourPoint_NTT(
-    input clk,
-    input reset,
-    input [16:0] in0,  //Individual input points, each 8 bits wide
-    input [16:0] in1,
-    input [16:0] in2,
-    input [16:0] in3,
-    output reg [16:0] out0,  //Individual output points, each 8 bits wide
-    output reg [16:0] out1,
-    output reg [16:0] out2,
-    output reg [16:0] out3
+module ntt4point(
+    input clk, rst,
+    input [15:0] in0, in1, in2, in3,
+    output reg [15:0] out0, out1, out2, out3
 );
+    wire [15:0] q_val = 16'd7681;
+    wire [15:0] phi1 = 16'd1925;
+    wire [15:0] phi2 = 16'd3383;
+    wire [15:0] phi3 = 16'd6468;
+    wire [15:0] temp0, temp1, temp2, temp3;
+    wire [15:0] y0, y1, y2, y3;
 
-    // Modulus and primitive root
-    parameter q = 7681;
-    parameter phi = 1925;
-    parameter phi2 = 3383;
-    parameter phi3 = 6468; 
+    //first stage
+    ctbf u0(.in_up(in0), .in_down(in2), .twf(phi2), .q(q_val), .out_up(temp0), .out_down(temp2));
+    ctbf u1(.in_up(in1), .in_down(in3), .twf(phi2), .q(q_val), .out_up(temp1), .out_down(temp3));
 
-    // Internal registers
-    reg [16:0] x0, x1, x2, x3;
-    reg [16:0] y0, y1, y2, y3;
-    reg [16:0] w1, w2, w3;
-    reg [16:0] temp0, temp1, temp2, temp3;
+    //second stage
+    ctbf u2(.in_up(temp0), .in_down(temp1), .twf(phi1), .q(q_val), .out_up(y0), .out_down(y1));
+    ctbf u3(.in_up(temp2), .in_down(temp3), .twf(phi3), .q(q_val), .out_up(y2), .out_down(y3));
 
-    // Function for modular addition
-    function [16:0] mod_add;
-        input [16:0] a, b;
-        begin
-            mod_add = (a + b) % q;
-        end
-    endfunction
+    always @(posedge clk) begin
+      if (rst) begin
+        out0 <= 0;
+        out1 <= 0;
+        out2 <= 0;
+        out3 <= 0;
+      end else begin
+        out0 <= y0;   
+        out1 <= y2; //bit reversed order output
+        out2 <= y1; //converting to normal order
+        out3 <= y3; 
+      end
+    end  
+endmodule
 
-    // Function for modular subtraction
-    function [16:0] mod_sub;
-        input [16:0] a, b;
-        begin
-            mod_sub = (a >= b) ? (a - b) : (q + a - b);
-        end
-    endfunction
+module ctbf(
+  input [15:0] in_up, in_down, twf, q,
+  output [15:0] out_up, out_down
+);
+  wire [15:0] temp;
+  modmul mm(.a(in_down), .b(twf), .q(q), .y(temp));
+  modadd ma(.a(in_up), .b(temp), .q(q), .y(out_up));
+  modsub ms(.a(in_up), .b(temp), .q(q), .y(out_down)); 
+endmodule
 
-    // Function for modular multiplication
-    function [16:0] mod_mul;
-        input [16:0] a, b;
-        begin
-            mod_mul = (a * b) % q;
-        end
-    endfunction
+module modadd(
+    input [15:0] a, b, q,
+    output [15:0] y
+);
+    wire [16:0] sum;
+    assign sum = a + b;
+    assign y = (sum >= q) ? sum - q : sum;
+endmodule
 
-    // Butterfly operation
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            x0 <= 0;
-            x1 <= 0;
-            x2 <= 0;
-            x3 <= 0;
-            y0 <= 0;
-            y1 <= 0;
-            y2 <= 0;
-            y3 <= 0;
-            out0 <= 0;
-            out1 <= 0;
-            out2 <= 0;
-            out3 <= 0;
-        end else begin
-            // Load input values
-            x0 <= in0;
-            x1 <= in1;
-            x2 <= in2;
-            x3 <= in3;
-            
-            // Precompute powers of omega
-            w1 <= phi;
-            w2 <= phi2;
-            w3 <= phi3;
-            
-            // First stage of butterfly
-            temp0 <= mod_add(x0, mod_mul(x2, phi2));
-            temp2 <= mod_sub(x0, mod_mul(x2, phi2));
-            temp1 <= mod_add(x1, mod_mul(x3, phi2));
-            temp3 <= mod_sub(x1, mod_mul(x3, phi2));
-            
-            
-            
-            // Second stage of butterfly
-            y0 <= mod_add(temp0, mod_mul(temp1, phi));
-            y1 <= mod_sub(temp0, mod_mul(temp1, phi));
-            y2 <= mod_add(temp2, mod_mul(temp3, phi3));
-            y3 <= mod_sub(temp2, mod_mul(temp3, phi3));
-            
-            // Store output values
-            out0 <= y0;
-            out1 <= y1;
-            out2 <= y2;
-            out3 <= y3;
-        end
-    end
+module modsub(
+    input [15:0] a, b, q,
+    output [15:0] y
+);
+    wire [16:0] qplusa;
+    assign qplusa = q + a;
+    assign y = (a >= b) ? (a - b) : (qplusa - b);
+endmodule
+
+module modmul(
+    input [15:0] a, b, q,
+    output [15:0] y
+);
+    wire [31:0] result;
+    assign result = a * b;
+    assign y = result % q;
 endmodule
